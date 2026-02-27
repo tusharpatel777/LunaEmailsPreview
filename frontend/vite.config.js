e523e5fbc4f781086f308b72d6bf62c9226a8a25
Tushar Patel <tussipatel826@gmail.com>
2026-02-27 12:40:29 +0530

    ready-for-deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://lunaemailspreview.onrender.com',
         changeOrigin: true,
       },
     },
